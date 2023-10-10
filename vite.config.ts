4f9c5fe77137837d0597b8bc59d9e26bdf2f15c3
ichiin <philippeduvalts2@gmail.com>
2023-10-10 18:03:30 +0200

    Edited config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   resolve: {
-    extensions: ['.js', '.mjs']
+    extensions: ['.js', '.mjs', '.ts', '.tsx']
   },
   plugins: [react()],
 })
