11f236388458cafc5c5fa3291430159a322ee1ac
ichiin <philippeduvalts2@gmail.com>
2023-10-15 08:33:13 +0200

    [Project] Set up formatting/linting, removed router for one pager website

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,24 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:node:recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
+    "class-methods-use-this": "off",
+    "func-names": "off",
+    "no-console": "off",
+    "no-process-exit": "off",
+    "no-unused-vars": "warn",
+    "object-shorthand": "off",
+    "prettier/prettier": "error",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
---
+++ src/containers/App/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "../Home/Home";
+import Projects from "../Projects/Project";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Projects />
+    </>
+  );
+}
+
+export default App;
